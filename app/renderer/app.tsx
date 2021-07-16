46e6fcc34ddce4f0db4204a138d1ba86ce862a2a
HasebeAya <1264051408@qq.com>
2021-07-16 17:49:36 +0800

    feat(css): 添加reset.css和normalize.css

--- app/renderer/app.tsx
+++ app/renderer/app.tsx
@@ -3,14 +3,17 @@
  * @Date: 2021-07-14 15:30:27
  * @Description: 渲染进程主要的js
  * @LastEditors: HasebeAya
- * @LastEditTime: 2021-07-16 09:33:57
+ * @LastEditTime: 2021-07-16 17:48:47
  */
 
 import React from 'react'
 
 import ReactDOM from 'react-dom'
-
+// router
 import Router from './router'
+// css
+import 'reset-css'
+import 'normalize.css'
 
 const rootSelector = '#root'
 
